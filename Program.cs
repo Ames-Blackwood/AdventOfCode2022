68e22b68cf4668c265b8f4076aed0ac607184178
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-11-16 22:44:56 -0600

    Init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿# nullable disable
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = CreateHostBuilder(args).Build();
+
+Application app = host.Services.GetRequiredService<Application>();
+
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices(
+            (_, services) => services
+                .AddSingleton<Application, Application>()
+                .AddSingleton<IAppConfig, AppConfig>());
+}
+
+interface IAppConfig
+{
+    string Setting { get; }
+}
+
+class AppConfig : IAppConfig
+{
+    public string Setting { get; }
+
+    public AppConfig(ILogger<AppConfig> logger)
+    {
+        logger.Log(LogLevel.Information, "AppConfig constructed");
+    }
+}
+
+class Application
+{
+    readonly IAppConfig config;
+    public Application(IAppConfig config, ILogger<Application> logger)
+    {
+        this.config = config;
+        logger.Log(LogLevel.Information, "Application constructed");
+    }
+}
