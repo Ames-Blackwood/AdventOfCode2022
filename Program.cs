09f82592624b227fbc790e15bc2141e79e0892e5
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-12-02 00:48:15 -0600

    Two diverging approaches.

--- Program.cs
+++ Program.cs
@@ -35,17 +35,21 @@ class Application
         int currentElf = 1;
         decimal value;
         bool isValue;
+        Stack<decimal> topCalories = new Stack<decimal>();
+        List<decimal> allCaloriesList = new List<decimal>();
 
         foreach (var line in lines)
         {
             isValue = Decimal.TryParse(line, out value);
             if (!isValue)
             {
+                allCaloriesList.Add(currentCalories);
                 elfCaloryTotal.Add(currentElf,currentCalories);
                 if (currentCalories > maxCalories)
                 {
                     maxCarryingElf = currentElf;
                     maxCalories = currentCalories;
+                    topCalories.Push(maxCalories);
                 }
                 currentElf++;
                 currentCalories = 0;
@@ -60,12 +64,33 @@ class Application
         {
             maxCarryingElf = currentElf;
             maxCalories = currentCalories;
+            topCalories.Push(maxCalories);
         }
 
         Decimal result;
-        _ = elfCaloryTotal.TryGetValue(maxCarryingElf, out result);
-        Console.WriteLine($"Calories carried by the elf with the most: {result}");
+        result = SumTopThree(topCalories);
+        Decimal result1 = allCaloriesList.OrderBy(inP => -inP).Take(3).Sum();
+        Console.WriteLine($"Calories carried by the 3 elves with the most by stack: {result}");
+        Console.WriteLine($"Calories carried by the 3 elves with the most by List: {result1}");
         Console.Write("Press any key to exit.");
         Console.ReadKey();
+    }
+
+    private Decimal SumTopThree(Stack<Decimal> inP)
+    {
+        var flagIncluded = (int count) => count < 3 ? "*" : "";
+        Decimal result = 0;
+        int count = 0;
+        Decimal tmp;
+        while (inP.TryPop(out tmp))
+        {
+            _logger.LogInformation($"{tmp}{flagIncluded(count)}");
+            if (count < 3)
+            {
+                result +=tmp;
+                count ++;
+            }
+        }
+        return result;
     }
 }
