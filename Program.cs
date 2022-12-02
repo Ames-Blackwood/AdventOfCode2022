dc18bba0a7d74155e458dc0aae9f3d63e19b4acf
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-12-02 00:16:30 -0600

    Part1 with test data.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 var host = CreateHostBuilder(args).Build();
 
 Application app = host.Services.GetRequiredService<Application>();
+app.Process();
 
 static IHostBuilder CreateHostBuilder(string[] args)
 {
@@ -21,6 +23,49 @@ class Application
     public Application(ILogger<Application> logger)
     {
         _logger = logger;
-        _logger.Log(LogLevel.Information, "Application constructed");
+    }
+
+    public void Process()
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"./input.txt");
+        Dictionary<int, decimal> elfCaloryTotal = new Dictionary<int, decimal>();
+        decimal maxCalories = 0;
+        decimal currentCalories = 0;
+        int maxCarryingElf = 1;
+        int currentElf = 1;
+        decimal value;
+        bool isValue;
+
+        foreach (var line in lines)
+        {
+            isValue = Decimal.TryParse(line, out value);
+            if (!isValue)
+            {
+                elfCaloryTotal.Add(currentElf,currentCalories);
+                if (currentCalories > maxCalories)
+                {
+                    maxCarryingElf = currentElf;
+                    maxCalories = currentCalories;
+                }
+                currentElf++;
+                currentCalories = 0;
+            }
+            else {
+                currentCalories += value;
+            }
+        }
+
+        elfCaloryTotal.Add(currentElf,currentCalories);
+        if (currentCalories > maxCalories)
+        {
+            maxCarryingElf = currentElf;
+            maxCalories = currentCalories;
+        }
+
+        Decimal result;
+        _ = elfCaloryTotal.TryGetValue(maxCarryingElf, out result);
+        Console.WriteLine($"Calories carried by the elf with the most: {result}");
+        Console.Write("Press any key to exit.");
+        Console.ReadKey();
     }
 }
