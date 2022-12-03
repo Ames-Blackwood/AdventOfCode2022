18869d4963766cc8b341294b6f5d89e97c72a81b
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-12-02 20:24:01 -0600

    Resetting to neutral.

--- Program.cs
+++ Program.cs
@@ -28,48 +28,11 @@ class Application
     public void Process()
     {
         string[] lines = System.IO.File.ReadAllLines(@"./input.txt");
-        Dictionary<int, decimal> elfCaloryTotal = new Dictionary<int, decimal>();
-        decimal maxCalories = 0;
-        decimal currentCalories = 0;
-        int maxCarryingElf = 1;
-        int currentElf = 1;
-        decimal value;
-        bool isValue;
-        List<decimal> allCaloriesList = new List<decimal>();
 
         foreach (var line in lines)
         {
-            isValue = Decimal.TryParse(line, out value);
-            if (!isValue)
-            {
-                allCaloriesList.Add(currentCalories);
-                elfCaloryTotal.Add(currentElf,currentCalories);
-                if (currentCalories > maxCalories)
-                {
-                    maxCarryingElf = currentElf;
-                    maxCalories = currentCalories;
-                }
-                currentElf++;
-                currentCalories = 0;
-            }
-            else {
-                currentCalories += value;
-            }
         }
 
-        elfCaloryTotal.Add(currentElf,currentCalories);
-        if (currentCalories > maxCalories)
-        {
-            maxCarryingElf = currentElf;
-            maxCalories = currentCalories;
-        }
-
-        List<Decimal> top3 = allCaloriesList.OrderBy(inP => -inP).Take(3).ToList();
-        foreach (var top in top3){
-            Console.WriteLine(top);
-        }
-        Decimal result = top3.Sum();
-        Console.WriteLine($"Calories carried by the 3 elves with the most by List: {result}");
         Console.Write("Press any key to exit.");
         Console.ReadKey();
     }
