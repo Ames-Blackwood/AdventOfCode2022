125d9a32d55e7b2a264301043559ebc364a315a3
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-12-06 22:46:05 -0600

    Removing old calculations

--- Logic/Calculations.cs
+++ Logic/Calculations.cs
@@ -2,13 +2,6 @@ namespace Advent.Logic
 {
     public static class Calculations 
     {
-        public static Func<char?, int> CalculatePriority = (value) => 
-        {
-            if (value is null) throw new ArgumentNullException(nameof(value));
-            int asciiValue = (int) value;
-            return asciiValue > 90
-                ? asciiValue - 96
-                : asciiValue - 38;
-        };
+
     }
 }
