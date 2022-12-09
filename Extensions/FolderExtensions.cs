74eb41691cbd69880677eb3f5183a07586657a37
Ames Blackwood (they/them) <ames.blackwood@gmail.com>
2022-12-08 22:40:47 -0600

    Adding a parent pointer to the folder.

--- Extensions/FolderExtensions.cs
+++ Extensions/FolderExtensions.cs
@@ -6,7 +6,7 @@ namespace Advent.Extensions
     {
         public static bool TryAddFolder(this Folder parent, string newFolderName)
         {
-            return parent.Folders.TryAdd(newFolderName, new Folder());
+            return parent.Folders.TryAdd(newFolderName, new Folder { Parent = parent });
         }
 
         public static bool TryAddFile(this Folder parent, string newFileName, decimal size)
--- Models/Folder.cs
+++ Models/Folder.cs
@@ -2,6 +2,7 @@ namespace Advent.Models
 {
     public class Folder
     {
+        public Folder? Parent { get; set; }
         public IDictionary<string,decimal> Files { get; set; } = new Dictionary<string,decimal>();
         public IDictionary<string,Folder> Folders {get; set; } = new Dictionary<string,Folder>();
     }
